da94b8c3350a5ccec2b5e6ad64e72f5b878e215d
nkosig <nkosig2@gmail.com>
2018-11-08 19:03:49 +0200

    Initial commit

---
+++ src/main/scala/com/allaboutscala/chapter/one/tutorial_06/HelloWorldWithArguments.scala
@@ -0,0 +1,8 @@
+package com.allaboutscala.chapter.one.tutorial_06
+
+object HelloWorldWithArguments extends App {
+  println("Hello World with Arguments Scala Application")
+
+  println("Command line arguments are:")
+  println(args.mkString(", "))
+}
---
+++ src/main/scala/com/allaboutscala/chapter/one/tutorial_07/HelloWorldWithArgumentsDebug.scala
@@ -0,0 +1,8 @@
+package com.allaboutscala.chapter.one.tutorial_07
+
+object HelloWorldWithArgumentsDebug extends App {
+  println("Hello World with Arguments Scala Application")
+
+  println("Command line arguments are:")
+  println(args.mkString(", "))
+}
